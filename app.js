5bea81fc70566ca0a53b0fa14d06fca48103193a
xkcm <42122011+xkcm@users.noreply.github.com>
2022-11-24 20:49:41 +0100

    KP-958 Refactoring and bug fixes (#1)

--- app.js
+++ app.js
@@ -1,13 +1,97 @@
 const { bootstrap } = require("@kaholo/plugin-library");
-const { runScheduler, checkActiveLicense, runCurl,  getRunItemIds, getRunItems } = require("./leapwork-api");
+
+const leapworkService = require("./leapwork-service");
 const autocomplete = require("./autocomplete");
+const { execCommand } = require("./helpers");
+
+async function runScheduler(params) {
+  const {
+    leapworkUrl,
+    accessKey,
+    id,
+    timeout,
+    variables,
+  } = params;
+
+  leapworkService.validateLeapworkUrl(leapworkUrl);
+
+  const runId = await leapworkService.postScheduler(leapworkUrl, accessKey, id, variables);
+  await leapworkService.waitForSchedulerToEnd({
+    leapworkUrl,
+    accessKey,
+    id,
+    timeout,
+  });
+  return leapworkService.getSchedulerResult(leapworkUrl, accessKey, runId);
+}
+
+async function checkActiveLicense(params) {
+  const {
+    leapworkUrl,
+    accessKey,
+    checkExpiry,
+  } = params;
+
+  leapworkService.validateLeapworkUrl(leapworkUrl);
+
+  const license = await leapworkService.getActiveLicense(leapworkUrl, accessKey);
+  console.info(`\nResponse: ${JSON.stringify(license)}\n`);
+
+  if (!checkExpiry) {
+    return license;
+  }
+
+  if (license.ExpireDays > 0) {
+    return `License expires in ${license.ExpireDays} days!`;
+  }
+  throw new Error("License expired!");
+}
+
+async function runCurl(params) {
+  const { command } = params;
+
+  const newCommand = `${command} -s`;
+  const { stdout: result } = await execCommand(newCommand);
+
+  try {
+    return JSON.parse(result.replace(/\$id/g, "id"));
+  } catch {
+    return result;
+  }
+}
+
+async function getRunItemIds(params) {
+  const {
+    leapworkUrl,
+    accessKey,
+    runId,
+  } = params;
+
+  leapworkService.validateLeapworkUrl(leapworkUrl);
+
+  const result = await leapworkService.getItemIds(leapworkUrl, accessKey, runId);
+  console.info(`\nResponse: ${JSON.stringify(result)}\n`);
+  return result;
+}
+
+async function getRunItems(params) {
+  const {
+    leapworkUrl,
+    accessKey,
+    runItemId,
+  } = params;
+
+  leapworkService.validateLeapworkUrl(leapworkUrl);
+
+  const result = await leapworkService.groupItems(leapworkUrl, accessKey, runItemId);
+  console.info(`\nResponse: ${JSON.stringify(result)}\n`);
+  return result;
+}
 
 module.exports = bootstrap({
   runScheduler,
   checkActiveLicense,
   getRunItemIds,
   getRunItems,
-  runCurl
-}, {
-  ...autocomplete,
-});
+  runCurl,
+}, autocomplete);
--- autocomplete.js
+++ autocomplete.js
@@ -1,10 +1,34 @@
-const { getSchedulers } = require("./helpers");
+const leapworkService = require("./leapwork-service");
 
 async function listSchedulers(query, params) {
-    const schedulers = await getSchedulers(params.leapworkURL, params.accessKey);
-    return schedulers;
+  const {
+    leapworkUrl,
+    accessKey,
+  } = params;
+
+  const schedulersData = await leapworkService.getSchedulers(leapworkUrl, accessKey);
+
+  const schedulers = Object
+    .values(schedulersData)
+    .filter((scheduler) => scheduler.Title)
+    .map(({ Id, Title }) => ({ id: Id, value: Title }));
+
+  return filterAutocompleteItemsByQuery(schedulers, query);
+}
+
+function filterAutocompleteItemsByQuery(autocompleteItems, query) {
+  if (!query) {
+    return autocompleteItems;
+  }
+
+  const lowerCaseQuery = query.toLowerCase();
+
+  return autocompleteItems.filter(({ value, id }) => (
+    value.toLowerCase().includes(lowerCaseQuery)
+    || id.toLowerCase().includes(lowerCaseQuery)
+  ));
 }
 
 module.exports = {
-    listSchedulers,
+  listSchedulers,
 };
